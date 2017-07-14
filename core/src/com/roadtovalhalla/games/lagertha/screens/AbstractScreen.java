f371aa111ea7ae21826f4f7194702186f20941e8
jonasbazzi <jonas.bazzi@despegar.com>
2017-07-13 21:34:37 -0300

    Abstract Screen

---
+++ core/src/com/roadtovalhalla/games/lagertha/screens/AbstractScreen.java
@@ -0,0 +1,34 @@
+package com.roadtovalhalla.games.lagertha.screens;
+
+import com.badlogic.gdx.Screen;
+import com.roadtovalhalla.games.lagertha.adapter.MainGame;
+
+public abstract class AbstractScreen implements Screen {
+
+	protected MainGame game;
+	
+	public AbstractScreen(MainGame game){
+		this.game = game;
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
